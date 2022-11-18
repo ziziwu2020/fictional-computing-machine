9975662a4a2e0886ad433c6c945129117ecc7280
zzw_git <zzw318@hotmail.com>
2022-11-18 15:12:54 +0800

    new measurements for zero covid19 in Beijing

--- readme.bat
+++ readme.bat
@@ -1,4 +1,23 @@
-git is a distributed version control system.
-git is a free software distributed under the GPL.
-it is my first time for me to use git.
-中文 can be applied in context. 
+[33mcommit bf25878d0e9073dc62b6fdb63c40736ffc002d4f[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: ziziwu2021 <zzw318@hotmail.com>
+Date:   Tue Nov 16 17:01:06 2021 +0800
+
+    the right path
+
+[33mcommit 259655fcaee755ef387cfce2a10fb3ac9dafc682[m
+Author: ziziwu2021 <zzw318@hotmail.com>
+Date:   Tue Nov 16 16:16:05 2021 +0800
+
+    测试中文
+
+[33mcommit 7665a2a4520c1c8eb930d0725eafa8bfafbdc644[m
+Author: ziziwu2021 <zzw318@hotmail.com>
+Date:   Tue Nov 16 16:10:46 2021 +0800
+
+    append distributed and GPL
+
+[33mcommit 755fe023f6db37e7c5fa205252536c0b0dd5fac8[m
+Author: ziziwu2021 <zzw318@hotmail.com>
+Date:   Tue Nov 16 16:03:34 2021 +0800
+
+    wrote a readme.bat
